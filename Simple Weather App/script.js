2db8f4b23ebee9d147c3f5e20d91107da4adeaf6
Norika Heathdale <bezantvibrant@gmail.com>
2024-11-26 14:19:16 +1000

    Update script.js

--- Simple Weather App/script.js
+++ Simple Weather App/script.js
@@ -13,3 +13,14 @@
     <ul class="cities"></ul>
   </div>
 </section>
+<li class="city">
+  <h2 class="city-name" data-name="...">
+    <span>...</span>
+    <sup>...</sup>
+  </h2>
+  <span class="city-temp">...<sup>°C</sup></span>
+  <figure>
+    <img class="city-icon" src="..." alt="...">
+    <figcaption>...</figcaption>
+  </figure>
+</li>
