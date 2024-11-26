6599692c6c24277243871735b16f16113e7a6563
Norika Heathdale <bezantvibrant@gmail.com>
2024-11-26 14:18:43 +1000

    Create script.js

---
+++ Simple Weather App/script.js
@@ -0,0 +1,15 @@
+<section class="top-banner">
+  <div class="container">
+    <h1 class="heading">Simple Weather App</h1>
+    <form>
+      <input type="text" placeholder="Search for a city" autofocus>
+      <button type="submit">SUBMIT</button>
+      <span class="msg"></span>
+    </form>
+  </div>
+</section>
+<section class="ajax-section">
+  <div class="container">
+    <ul class="cities"></ul>
+  </div>
+</section>
